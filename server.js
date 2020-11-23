30cfa89b109a0436072fe9c8d2bb8892eb781199
JP <jpjones22@gmail.com>
2020-11-22 19:57:28 -0800

    Attempting to Read Paginated data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict'
 const express = require('express')
 const superagent = require('superagent');
 const dotenv = require('dotenv');
@@ -13,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const DATABASE_URL = process.env.DATABASE_URL;
 const client = new pg.Client(DATABASE_URL);
+const RAWG_API_KEY = process.env.RAWG_API_KEY;
 
 
 app.use(cors());
@@ -36,6 +38,33 @@ client.connect()
 
 //FUNCTIONS
 
+
+
 function homePage(req, res) {
-  res.send('Never you mind this string.  WE are just getting started.');
+  let giganticArray = [];
+  let outOfPages = false;
+  let pageNum = 1;
+  const platformId = '105';
+  for (let i = 1; i < 3; i++) {
+    console.log(pageNum, giganticArray, outOfPages);
+    const URL = `https://api.rawg.io/api/games?key=${RAWG_API_KEY}&platforms=105&page_size=2&ordering=name&page=${pageNum}`;
+    console.log('page Number before', pageNum);
+    superagent(URL)
+
+      .then(data => {
+        // console.log('page Number', pageNum);
+        // console.log('data', data.body.results);
+        giganticArray.push('Blap');//data.body.results);
+        return data;
+
+      })
+      .then(console.log('gigantic array length giganticArray', giganticArray.length))
+      .then(data => res.send(data.body.results))
+      .catch(err => {
+        console.log('Unable to acess RAWG games database. Or reached end of pages, you decide.');
+        outOfPages = true;
+      });
+    pageNum++;
+  }
+
 }
