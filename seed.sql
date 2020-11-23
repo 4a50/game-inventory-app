aee693fac704e03956579dab907cfd8b288ced5d
Simon Panek <paneks19@gmail.com>
2020-11-23 15:08:23 -0800

    Create initial database and provide test data

---
+++ seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO gameInventoryData (title, category, condition, description, game_count, game_id, image_url, notes, platform_id, platform_name, publisher, release_data, video_url) VALUES ('title-test-1', 'category-test-1', 'condition-test-1', 'description-test-1', '77777771', '88888881', 'image_url-test-1', 'notes-test-1', '99999991', 'platform-name-test-1', 'publisher-test-1', '2020-01-01', 'video_url-test-1');
+
+INSERT INTO gameInventoryData (title, category, condition, description, game_count, game_id, image_url, notes, platform_id, platform_name, publisher, release_data, video_url) VALUES ('title-test-2', 'category-test-2', 'condition-test-2', 'description-test-2', '77777772', '88888882', 'image_url-test-2', 'notes-test-2', '99999992', 'platform-name-test-2', 'publisher-test-2', '2020-01-02', 'video_url-test-2');
+
+INSERT INTO gameInventoryData (title, category, condition, description, game_count, game_id, image_url, notes, platform_id, platform_name, publisher, release_data, video_url) VALUES ('title-test-3', 'category-test-3', 'condition-test-3', 'description-test-3', '77777773', '88888883', 'image_url-test-3', 'notes-test-3', '99999993', 'platform-name-test-3', 'publisher-test-3', '2020-01-03', 'video_url-test-3');
+
+
+INSERT INTO apiData (title, category, condition, description, game_count, game_id, image_url, notes, platform_id, platform_name, publisher, release_data, video_url) VALUES ('title-data-1', 'category-data-1', 'condition-data-1', 'description-data-1', '77777701', '88888801', 'image_url-data-1', 'notes-data-1', '99999901', 'platform-name-data-1', 'publisher-name-data-1', '2020-02-01', 'video_url-data-1');
+
+INSERT INTO apiData (title, category, condition, description, game_count, game_id, image_url, notes, platform_id, platform_name, publisher, release_data, video_url) VALUES ('title-data-2', 'category-data-2', 'condition-data-2', 'description-data-2', '77777702', '88888802', 'image_url-data-2', 'notes-data-2', '99999902', 'platform-name-data-2', 'publisher-name-data-2', '2020-02-02', 'video_url-data-2');
+
+INSERT INTO apiData (title, category, condition, description, game_count, game_id, image_url, notes, platform_id, platform_name, publisher, release_data, video_url) VALUES ('title-data-3', 'category-data-3', 'condition-data-3', 'description-data-3', '77777703', '88888803', 'image_url-data-3', 'notes-data-3', '99999903', 'platform-name-data-3', 'publisher-name-data-3', '2020-02-03', 'video_url-data-3');
